9f7e93e1bda53d0e0bbfdd1ae362a2a13f439051
vijayaphadnis123 <vijayaphadnis@gmail.com>
2021-07-14 20:46:57 +0000

    added scaffold files

---
+++ test_hello.py
@@ -0,0 +1,17 @@
+from hello import toyou, add, subtract
+
+def setup_function(function):
+	print ("Running Setup: %s" % function.__name__)
+	function.x = 10
+
+def teardowm_function(function):
+	print ("Running Teardowm: %s" % function.__name__)
+	del function.x
+
+#Will cause an assert error
+#def test_hello_add():
+#	assert add (test_hello_add.x) == 12
+
+def test_hello_subtract():
+	assert subtract (test_hello_subtract.x) == 9
+
